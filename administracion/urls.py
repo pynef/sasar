0e90d8b910333faabc9ff0ea862228bfa0258488
Arturo Black <arturo.black0@gmail.com>
2015-03-21 18:18:33 -0500

    change dashboar template

--- administracion/urls.py
+++ administracion/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('administracion.views', 
-    url(r'administracion^/$', 'home', name='home')
-    )
+    url(r'administracion/$', 'home', name='home'),
+    url(r'administracion/apertura$', 'apertura', name='apertura'),
+)
--- administracion/views.py
+++ administracion/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home(request):
 	return render(request,'frontend/index.html')
+
+def apertura(req):
+    return render(req, 'backend/apertura.html')
