7f57f6cc4112b247e681e89075bee0abcee59ac9
PyNef <nefinef@gmail.com>
2015-03-21 17:18:03 -0500

    pagina de socios

--- frontend/urls.py
+++ frontend/urls.py
@@ -2,5 +2,8 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('frontend.views',
 	url(r'^$','index',name='index'),
+	url(r'^/$','inicio',name='inicio'),
 	url(r'^home/$','home',name='home'),
+	url(r'^nosotros/$','nosotros',name='nosotros'),
+	url(r'^socio/(?P<dni>\d+)$','socio',name='socio'),
 )
--- frontend/views.py
+++ frontend/views.py
@@ -5,11 +5,29 @@ from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.shortcuts import get_object_or_404
 from legion.decoratos import administrador_login,backend_login,frontend_login
+from backend.models import Socio
 
-
+def inicio(request):
+    return render(request,'frontend/index.html')
 
 def home(request):
     return render(request,'frontend/index.html')
+
+def nosotros(request):
+    usuarios = Socio.objects.all()
+    print usuarios
+    cntxt={
+        'usuarios':usuarios,
+    }
+    return render(request,'frontend/nosotros.html',cntxt)
+
+def socio(request,dni):
+    usuario = Socio.objects.get(dni=dni)
+    print usuario.first_name
+    cntxt={
+        'usuario':usuario,
+    }
+    return render(request,'frontend/socio.html',cntxt)
 
 def index(request):
     if request.user.is_authenticated() :
@@ -19,7 +37,7 @@ def index(request):
         elif request.user.groups.filter(name='backend'):
             return HttpResponseRedirect('/panel/')
         elif request.user.groups.filter(name='frontend'):
-            return HttpResponseRedirect('/home/')
+            return HttpResponseRedirect('/')
         else:
             return HttpResponseRedirect('/logout')
             print "salir no usuario"
